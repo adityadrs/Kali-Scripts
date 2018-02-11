785f9c7ae7946b0b6338221d7d6973b70ec46786
root <adityadrs@gmail.com>
2018-02-11 18:09:07 +0530

    additional info added

--- Kali-fresh-install.sh
+++ Kali-fresh-install.sh
@@ -2767,6 +2767,13 @@ popd >/dev/null
 apt-get install imwheel\
   || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
 touch ~/.imwheelrc
+#change the marked values to change the mouse scroll speed
+#None,      Up,   Button4, 3 <-- change this
+#None,      Down, Button5, 3 <-- change this
+#Control_L, Up,   Control_L|Button4
+#Control_L, Down, Control_L|Button5
+#Shift_L,   Up,   Shift_L|Button4
+#Shift_L,   Down, Shift_L|Button5
 echo '.*" \nNone,      Up,   Button4, 3 \nNone,      Down, Button5, 3 \nControl_L, Up,   Control_L|Button4\nControl_L, Down, Control_L|Button5 \nShift_L,   Up,   Shift_L|Button4 \nShift_L,   Down, Shift_L|Button5 \n'> ~/.imwheelrc
 echo 'imwheel --kill --buttons "4 5"'>> .bashrc
 popd >/dev/null
